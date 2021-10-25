406d4d3dea2592ba672c259df15d5c65f52e5a12
kubonagi <nagi3ringo@gmail.com>
2021-10-25 15:54:21 +0900

    10.25 intro.order.Switch

---
+++ src/intro/order/Switch.java
@@ -0,0 +1,24 @@
+package intro.order;
+
+
+//switch命令 その１
+public class Switch {
+    public static void main(String[] args) {
+        char ch = 'A';
+
+        switch (ch) {
+            case 'A':
+                System.out.println('A');
+                break;
+            case 'B':
+                System.out.println('B');
+                break;
+            case 'C':
+                System.out.println('C');
+                break;
+            default:
+                System.out.println("Other");
+                break;
+        }
+    }
+}
